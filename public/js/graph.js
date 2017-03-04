d998cda2e5db6886de672f086afc71266be87943
dannycho7 <dannycho7@gmail.com>
2017-03-04 06:15:37 -0800

    Minor styling changes

--- public/js/graph.js
+++ public/js/graph.js
@@ -4,9 +4,10 @@ function load_nodes(){
 		var json_data = data.nodes[node_entry];
 		var node = document.createElement('div');
 		node.id = json_data.class_name;
+		node.title = "Class name: " + node.id;
 		node.className = "circle";
-		node.style.left = parseInt(json_data.coordinates.x)  + Math.floor(Math.random()*30) + 'px';
-		node.style.top = parseInt(json_data.coordinates.y)  + Math.floor(Math.random()*30) + 'px';	
+		node.style.left = parseInt(json_data.coordinates.x)  + Math.floor(Math.random()*20) + 'px';
+		node.style.top = parseInt(json_data.coordinates.y)  + Math.floor(Math.random()*20) + 'px';	
 		node.style.height = 30 * parseFloat(json_data.size) + 'px';
 		node.style.width = 30 * parseFloat(json_data.size) + 'px';	
 		container.appendChild(node);
